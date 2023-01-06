6b05eb7e9791eefce4b1372677a2620e8c82818e
Igor Gilmutdinov <bladoff@gmail.com>
2023-01-07 02:06:16 +0300

    Init commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+            nodePackages.npm
+            nodejs
+        ];
+
+        shellHook = ''
+            export PATH=$PWD/.node_modules/bin:$PATH
+        '';
+    };
+  };
+}
