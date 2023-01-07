9d2f0622f9ae8111b4150c32bfb62bd16a3b1276
Igor Gilmutdinov <bladoff@gmail.com>
2023-01-07 05:25:55 +0300

    Linter

--- src/logic.js
+++ src/logic.js
@@ -1,33 +1,33 @@
-import { greetUser } from "./cli.js";
 import readlineSync from 'readline-sync';
+import greetUser from './cli.js';
 
 export const gameLogic = (greeting, generateQuestion, checkAnswer) => {
-	const name = greetUser();
-	console.log(greeting);
+  const name = greetUser();
+  console.log(greeting);
 
-	for (let i = 0; i < 3; i++) {
-		const question = generateQuestion();
-		console.log(`Question: ${question}`);
-		const a = readlineSync.question("Your answer: ");
-		const ans = checkAnswer(a);
-		if (ans.good) {
-			console.log("Correct!");
-		} else {
-			console.log(`'${a}' is wrong answer ;(. Correct answer was '${ans.otherwise}'.`);
-			console.log(`Let's try again, ${name}!`);
-			return
-		}
-	}
+  for (let i = 0; i < 3; i += 1) {
+    const question = generateQuestion();
+    console.log(`Question: ${question}`);
+    const a = readlineSync.question('Your answer: ');
+    const ans = checkAnswer(a);
+    if (ans.good) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${a}' is wrong answer ;(. Correct answer was '${ans.otherwise}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
 
-	console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!`);
 };
 
 export const compareToNumber = (ans, result) => {
-	const num = Number(ans);
-	const correct = num == result;
-	const otherwise = result;
-	return {
-		good: correct,
-		otherwise: otherwise,
-	};
+  const num = Number(ans);
+  const correct = num === result;
+  const otherwise = result;
+  return {
+    good: correct,
+    otherwise,
+  };
 };
